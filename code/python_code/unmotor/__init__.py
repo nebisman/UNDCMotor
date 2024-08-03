ae8dcc9784c27d0233058e8a4f3ec4e317987e91
Leonardo Bermeo <lbermeoc@unal.edu.co>
2024-08-03 12:02:47 -0500

    completing packaging

---
+++ code/python_code/unmotor/__init__.py
@@ -0,0 +1,3 @@
+from .controlsys import MotorSystemIoT
+from .identsys import get_models_prbs, get_static_model, prbs_open, step_open, get_fomodel_step
+from .controlsys import step_closed, set_controller, set_reference, set_pid, stairs_closed, profile_closed
