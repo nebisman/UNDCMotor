6fffb21d0a60a2fb2cb754965c4e46ba2c5a1a30
Leonardo Bermeo <lbermeoc@unal.edu.co>
2024-08-07 10:02:38 -0500

    example of connection settings included

---
+++ code/esp32_unmotor/connection_settings.h
@@ -0,0 +1,9 @@
+#define PLANT_NUMBER  "2020"                 // set the number of your plant
+#define THINGNAME "DCMotor_" PLANT_NUMBER
+const char WIFI_SSID[] = "UNAL";             // set your WiFi network
+const char WIFI_PASSWORD[] = "UNAL";         // set the password of your WiFi network
+const char BROKER[] = "192.168.0.3";         // set your broker address eg."18.204.70.207";  
+
+// login access
+const char USER[] = "unmotor";               // do not change
+const char PASSWORD[] = "unmotor";           // do not change
