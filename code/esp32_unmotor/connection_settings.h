bd27f6b566028771c20753103150d65adbeec2a7
Leonardo Bermeo <lbermeoc@unal.edu.co>
2024-08-28 18:23:02 -0500

    connection settings updated

--- code/esp32_unmotor/connection_settings.h
+++ code/esp32_unmotor/connection_settings.h
@@ -1,10 +1,9 @@
-#define PLANT_NUMBER  "PC-M-01"                 // set the number of your plant
+#define PLANT_NUMBER  "PC-M-XX"                 // set the number of your plant
 #define THINGNAME "DCMotor_" PLANT_NUMBER
 const char WIFI_SSID[] = "lab_control";             // set your WiFi network
 const char WIFI_PASSWORD[] = "lab_control";         // set the password of your WiFi network
 const char BROKER[] = "192.168.1.100";         // set your broker address eg."10.203.162.69";
 
 // login access
-const char USER[] = "unmotor1";                // do not change
-const char PASSWORD[] = "unmotor1";            // do not change
-//#define UNALCONNECTION                        // comment if your are outside the university
+const char USER[] = "unmotor";                // do not change
+const char PASSWORD[] = "unmotor";            // do not change
