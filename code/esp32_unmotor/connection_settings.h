8a3eb2eaa4f1dcfb955a01f6d1fea74ae8ef88ac
leonardo <lbermeoc@unal.edu.co>
2024-10-24 12:47:45 -0500

    functions updated for plattformio

--- code/esp32_unmotor/connection_settings.h
+++
@@ -1,9 +0,0 @@
-#define PLANT_NUMBER  "PC-M-XX"                 // set the number of your plant
-#define THINGNAME "DCMotor_" PLANT_NUMBER
-const char WIFI_SSID[] = "lab_control";             // set your WiFi network
-const char WIFI_PASSWORD[] = "lab_control";         // set the password of your WiFi network
-const char BROKER[] = "192.168.1.100";         // set your broker address eg."10.203.162.69";
-
-// login access
-const char USER[] = "unmotor";                // do not change
-const char PASSWORD[] = "unmotor";            // do not change
