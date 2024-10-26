2f84b0dc2d8cba59d0b0c81a860135bfd272f60d
Leonardo Bermeo <lbermeoc@unal.edu.co>
2024-10-25 21:20:43 -0500

    settings fixed

--- code/esp32_unmotor/src/connection_settings.h
+++ code/esp32_unmotor/src/connection_settings.h
@@ -1,12 +1,12 @@
 // MQTT settings
 #define  PLANT_NUMBER  "PC-M-XX"                 // set the number of your plant
 #define  THINGNAME "Motor_" PLANT_NUMBER
-#define  BROKER  "192.168.0.9"         // set your broker address eg."192.168.1.100";
-#define  PORT     8883                  //1883
+#define  BROKER  "192.168.1.100"         // set your broker address eg."192.168.1.100";
+#define  PORT     1883                  //1883
 
 // WiFi Network
-const char WIFI_SSID[] = "Apto414-2";             // set your WiFi network
-const char WIFI_PASSWORD[] = "Sinergy_2096_2";    // set the password of your WiFi network
+const char WIFI_SSID[] = "XXXXXXXX";             // set your WiFi network
+const char WIFI_PASSWORD[] = "XXXXXXXX";    // set the password of your WiFi network
 
 // login access
 const char USER[] = "unmotor";                // do not change
