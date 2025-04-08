be8b2b1781edb71b5fb7ff2f701b6edbee4e4459
Leonardo Bermeo <lbermeoc@unal.edu.co>
2025-04-08 15:41:34 -0500

    firmware updated

--- code/esp32_unmotor/src/connection_settings.h
+++ code/esp32_unmotor/src/connection_settings.h
@@ -1,12 +1,12 @@
 // MQTT settings
-#define  PLANT_NUMBER  "PC-M-9"                 // set the number of your plant
+#define  PLANT_NUMBER  "PC-M-10"                 // set the number of your plant
 #define  THINGNAME "Motor_" PLANT_NUMBER
 #define  BROKER  "192.168.1.100"         // set your broker address eg."192.168.1.100";
 #define  PORT     1883                  //1883
 
 // WiFi Network
 const char WIFI_SSID[] = "lab_control";             // set your WiFi network
-const char WIFI_PASSWORD[] = "lab_control";    // set the password of your WiFi networ
+const char WIFI_PASSWORD[] = "lab_control";    // set the password of your WiFi network
 // login access
 const char USER[] = "unmotor";                // do not change
 const char PASSWORD[] = "unmotor";            // do not change
